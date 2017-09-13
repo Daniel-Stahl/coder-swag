f4765de7d3410358ac9af84e96681131a93ed70a
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2017-09-13 16:10:33 -0700

    Created collection cells

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Daniel Stahl on 9/13/17.
+//  Copyright © 2017 Daniel Stahl. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
